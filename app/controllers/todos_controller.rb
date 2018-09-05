3a8e2c4cf8358362ccfd2bd3f5d62d82ce7b4b05
Rafeeq Ebrahim <cerafeeq@gmail.com>
2018-09-05 15:20:25 +0530

    Added authentication

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,15 +1,18 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :update, :destroy]
 
+  # [...]
   # GET /todos
   def index
-    @todos = Todo.all
+    # get current user todos
+    @todos = current_user.todos
     json_response(@todos)
   end
-
+  # [...]
   # POST /todos
   def create
-    @todo = Todo.create!(todo_params)
+    # create todos belonging to current user
+    @todo = current_user.todos.create!(todo_params)
     json_response(@todo, :created)
   end
 
@@ -34,7 +37,7 @@ class TodosController < ApplicationController
 
   def todo_params
     # whitelist params
-    params.permit(:title, :created_by)
+    params.permit(:title)
   end
 
   def set_todo
